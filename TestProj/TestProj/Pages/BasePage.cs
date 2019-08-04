0394987a49be62152ecd1a886eb0878c42120fd4
Loredana Moga <loredanamoga7@gmail.com>
2019-08-05 00:21:20 +0300

    added dependencies and proj structure

---
+++ TestProj/TestProj/Pages/BasePage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProj.Pages
+{
+    public class BasePage
+    {
+    }
+}
---
+++ TestProj/TestProj/Tests/BaseTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestProj.Tests
+{
+    public class BaseTest
+    {
+
+        public IWebDriver Driver;
+
+        public void InitializeDriver(bool isHeadless)
+        {
+            var options = new ChromeOptions();
+            options.SetLoggingPreference(LogType.Browser, LogLevel.All);
+            options.AddArgument("--start-maximized");
+            options.AddArgument("--ignore-certificate-errors");
+
+            if (isHeadless)
+            {
+                options.AddArgument("--headless");
+                options.AddArgument("--window-size=1920,1080");
+            }
+
+            Driver = new ChromeDriver(options);
+
+            Driver.Manage().Window.Maximize();
+          
+
+        }
+    }
+}
